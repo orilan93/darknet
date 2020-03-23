7e1615b5e25976e97d2f43dee29e2e90fdbe5b58
Espen Kalhagen <espenkal96@gmail.com>
2020-03-23 12:05:14 +0100

    Added oLRP

--- scripts/calc_validation_darknet.py
+++ scripts/calc_validation_darknet.py
@@ -10,18 +10,22 @@ import pandas as pd
 import math
 import numpy as np
 
-PROB_THRESHOLD = 0.25
-IOU_THRESH = 0.25
+PROB_THRESHOLD = 0.5
+IOU_THRESH = 0.5
 
 #PR curve calculation method, NB mutual exclusive
 EXPONENTAL_SCORE = False
 GAUSSIAN_SCORE =False
 CURVE_SMOOTHING = False
-ACCEPT_PARENT = True
+ACCEPT_PARENT = False
 
 POINT_INTER = False
 POINT_INTER_SKIP = 0.1 # 0.1 for 11 point inter
 
+USE_oLRP = True
+
+if USE_oLRP:
+    PROB_THRESHOLD = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument("tree_file", help="The xml tree file.")
@@ -166,224 +170,263 @@ def bb_intersection_over_union(boxA, boxB):
     # return the intersection over union value
     return iou
 
-#load some misc data
-resolution = (int(args.resolution.split("x")[0]),int(args.resolution.split("x")[1]))
-species_names=[]
-with open(args.names_file) as names_file:
-    csv_reader = csv.reader(names_file, delimiter=',')
-    species_names.extend(name[0] for name in csv_reader)
-
-#load tree
-tree = ET.parse(args.tree_file)
-root = tree.getroot()[0]
-root_class = Node(root.attrib["name"])
-species_nodes = {}
-species_nodes[root_class.name]=root_class
-for child in root:
-    depth_first(child,root_class)
-
-# Load results
-files = [f for f in os.listdir(args.results_folder)
-         if os.path.isfile(os.path.join(args.results_folder, f)) and f.split('.')[-1] == 'txt']
-predictions={}
-
-for file in files:
-    file_path = os.path.join(args.results_folder, file)
-    with open(file_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=' ')
-        species = functools.reduce(lambda a,b: a if a in file else b,species_names)
-        for row in csv_reader:
-            prediction = Prediction(row[0], species, row[1], row[2], row[3],row[4],row[5])
-            if(float(row[1]) < PROB_THRESHOLD): continue
-            predictions.setdefault(int(row[0]), [])
-            predictions[int(row[0])].append(prediction)
-print("Loaded predictions:")
-print(predictions)
-
-# Load Ground Truth
-labels = {}
-
-with open(args.manifest_file) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        label_file = row[0][:-3]+"txt"
-        image_id = get_image_id(label_file)
-        with open(label_file) as csv_label_file:
-            csv_label_reader = csv.reader(csv_label_file, delimiter=' ')
-            for row in csv_label_reader:
-                labels.setdefault(int(image_id),[])
-                labels[int(image_id)].append(Label(image_id, row[0],row[1], row[2], row[3],row[4]))
-
-print("Loaded labels:")
-print(labels)
-
-#calculate IOU
-
-#Assosicate labels and predictions
-for image_id, image_labels in labels.items():
-    for label in image_labels:
-        if predictions.get(image_id) == None: continue
-        best_iou = -1
-        best_pred = None
-        for pred in predictions[image_id]:
-            iou = bb_intersection_over_union(label.bbox, pred.bbox)
-            if iou > best_iou and iou > IOU_THRESH:
-                best_iou = iou
-                best_pred = pred
-        if(best_pred is not None):
-            label.iou = best_iou
-            label.prediction = best_pred
-
-            #print(f"im: {image_id} iou: {best_iou} species: {label.species} ")
-
-
 
 labels_flat = []
-for image_id, image_labels in labels.items():
-    for label in image_labels:
-        check_prediction(label)
-        labels_flat.append(label)
-        try:
-            predicted_species = label.prediction.species.name
-        except:
-            predicted_species = "n/a"
-        #print(f"im: {label.image_id} predicted: {label.predicted} iou: {label.iou} offset: {label.tree_offset} real_species: {label.species} prediceted: {predicted_species}")
-
-#Associate lables in from prediction
-for label in labels_flat:
-    if label.prediction is not None:
-        label.prediction.label = label
+species_names = []
 
 
-preds_flat = functools.reduce(add, predictions.values())
+step = 0.01
+if USE_oLRP: run = np.arange(0, 1, step)
+else: run = [PROB_THRESHOLD]
+LRPs = []
+for PROB_THRESHOLD in run:
 
-true_positives = list(filter(lambda e: e.predicted,labels_flat))
-false_negatives = list(filter(lambda e: not e.predicted, labels_flat))
-false_positive = list(filter(lambda e: e.label is None or not e.label.predicted, preds_flat))
+    species_names = []
+    labels_flat = []
 
-precision = len(true_positives)/(len(true_positives)+len(false_positive))
-print(f"Precision {precision}")
-recall = len(true_positives)/(len(true_positives)+len(false_negatives))
-print(f"Recall {recall}")
+    #load some misc data
+    resolution = (int(args.resolution.split("x")[0]),int(args.resolution.split("x")[1]))
+    with open(args.names_file) as names_file:
+        csv_reader = csv.reader(names_file, delimiter=',')
+        species_names.extend(name[0] for name in csv_reader)
+
+    #load tree
+    tree = ET.parse(args.tree_file)
+    root = tree.getroot()[0]
+    root_class = Node(root.attrib["name"])
+    species_nodes = {}
+    species_nodes[root_class.name]=root_class
+    for child in root:
+        depth_first(child,root_class)
+
+    # Load results
+    files = [f for f in os.listdir(args.results_folder)
+             if os.path.isfile(os.path.join(args.results_folder, f)) and f.split('.')[-1] == 'txt']
+    predictions={}
+
+    for file in files:
+        file_path = os.path.join(args.results_folder, file)
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=' ')
+            species = functools.reduce(lambda a,b: a if a in file else b,species_names)
+            for row in csv_reader:
+                prediction = Prediction(row[0], species, row[1], row[2], row[3],row[4],row[5])
+                if(float(row[1]) < PROB_THRESHOLD): continue
+                predictions.setdefault(int(row[0]), [])
+                predictions[int(row[0])].append(prediction)
+    #print("Loaded predictions:")
+    #print(predictions)
+
+    # Load Ground Truth
+    labels = {}
+
+    with open(args.manifest_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            label_file = row[0][:-3]+"txt"
+            image_id = get_image_id(label_file)
+            with open(label_file) as csv_label_file:
+                csv_label_reader = csv.reader(csv_label_file, delimiter=' ')
+                for row in csv_label_reader:
+                    labels.setdefault(int(image_id),[])
+                    labels[int(image_id)].append(Label(image_id, row[0],row[1], row[2], row[3],row[4]))
+
+    #print("Loaded labels:")
+    #print(labels)
+
+    #calculate IOU
+
+    #Assosicate labels and predictions
+    for image_id, image_labels in labels.items():
+        for label in image_labels:
+            if predictions.get(image_id) == None: continue
+            best_iou = -1
+            best_pred = None
+            for pred in predictions[image_id]:
+                iou = bb_intersection_over_union(label.bbox, pred.bbox)
+                if iou > best_iou and iou > IOU_THRESH:
+                    best_iou = iou
+                    best_pred = pred
+            if(best_pred is not None):
+                label.iou = best_iou
+                label.prediction = best_pred
+
+                #print(f"im: {image_id} iou: {best_iou} species: {label.species} ")
 
 
-#Calulate MAP
-preds_flat.sort(key=lambda e: e.prob)
-preds_flat.reverse()
-precision_list = []
 
-false_negatives = list(filter(lambda e: not e.predicted, labels_flat))
+    labels_flat = []
+    for image_id, image_labels in labels.items():
+        for label in image_labels:
+            check_prediction(label)
+            labels_flat.append(label)
+            try:
+                predicted_species = label.prediction.species.name
+            except:
+                predicted_species = "n/a"
+            #print(f"im: {label.image_id} predicted: {label.predicted} iou: {label.iou} offset: {label.tree_offset} real_species: {label.species} prediceted: {predicted_species}")
 
-for e in preds_flat:
+    #Associate lables in from prediction
+    for label in labels_flat:
+        if label.prediction is not None:
+            label.prediction.label = label
 
-    if(e.label is None or e.label.predicted == False): ##Not predicted
-        e.score = 0
+    try:
+        preds_flat = functools.reduce(add, predictions.values())
+    except:
+        print("No preductions, skipping")
+        continue
 
-    elif (e.label.tree_offset < 0):
-        offset = abs(e.label.tree_offset)
-        if(EXPONENTAL_SCORE):
-            e.score = (10**(1/(offset+1)))/10
-        elif(GAUSSIAN_SCORE):
-            #e^(-(x^2)/2)
-            e.score = math.exp(-(offset)**2/2)
-            pass
+    true_positives = list(filter(lambda e: e.predicted,labels_flat))
+    false_negatives = list(filter(lambda e: not e.predicted, labels_flat))
+    false_positive = list(filter(lambda e: e.label is None or not e.label.predicted, preds_flat))
+
+    precision = len(true_positives)/(len(true_positives)+len(false_positive))
+    print(f"Precision {precision}")
+    recall = len(true_positives)/(len(true_positives)+len(false_negatives))
+    print(f"Recall {recall}")
+
+    if(not USE_oLRP):
+        #Calulate MAP
+        preds_flat.sort(key=lambda e: e.prob)
+        preds_flat.reverse()
+        precision_list = []
+
+        false_negatives = list(filter(lambda e: not e.predicted, labels_flat))
+
+        for e in preds_flat:
+
+            if(e.label is None or e.label.predicted == False): ##Not predicted
+                e.score = 0
+
+            elif (e.label.tree_offset < 0):
+                offset = abs(e.label.tree_offset)
+                if(EXPONENTAL_SCORE):
+                    e.score = (10**(1/(offset+1)))/10
+                elif(GAUSSIAN_SCORE):
+                    #e^(-(x^2)/2)
+                    e.score = math.exp(-(offset)**2/2)
+                    pass
+                else:
+                    e.score=1
+
+            elif (e.label.tree_offset >= 0):
+                e.score = 1
+
+        for i in range(len(preds_flat)):
+            true_positives = list(filter(lambda e: e.label is not None and e.label.predicted,preds_flat[:i+1]))
+            false_positives =  list(filter(lambda e: e.label is None or not e.label.predicted, preds_flat[:i+1]))
+
+            #precision = len(true_positives) / (len(true_positives) + len(false_positives))
+            precision = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_positives))
+            recall = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_negatives))
+            #recall = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_negatives))
+            #print(recall)
+            predicted = preds_flat[i].label is not None and preds_flat[i].label.predicted
+
+            row = {"predicted":predicted,"precision":precision, "recall": recall}
+            precision_list.append(row)
+
+        #print(precision_list)
+
+
+        if(CURVE_SMOOTHING):
+            precision_list[0]["smooth"] = precision_list[0]["precision"]
+            for i in range(1, len(precision_list)-1):
+                '''
+                avg = 0
+                for j in range(-2,2):
+                    avg = avg+precision_list[i-j]["precision"]
+                avg = avg/5
+                '''
+                avg = (precision_list[i-1]["precision"]*10+precision_list[i]["precision"]+precision_list[i+1]["precision"]*10)/21
+
+                precision_list[i]["smooth"] = avg
+            precision_list[-1]["smooth"] = precision_list[-1]["precision"]
+
+
+            for e in precision_list:
+                e["precision"] = e["smooth"]
+
+        #do interpolation
+
+        if(POINT_INTER):
+            skip = POINT_INTER_SKIP
+            for r in np.arange(0,1,skip):
+                max =0
+                rows = [x for x in precision_list if( x["recall"] > r and x["recall"] <= (r + skip))]
+                for row in rows:
+                    if row["precision"]> max: max = row["precision"]
+                for row in rows:
+                    row["precision_inter"] = max
         else:
-            e.score=1
-
-    elif (e.label.tree_offset >= 0):
-        e.score = 1
-
-for i in range(len(preds_flat)):
-    true_positives = list(filter(lambda e: e.label is not None and e.label.predicted,preds_flat[:i+1]))
-    false_positives =  list(filter(lambda e: e.label is None or not e.label.predicted, preds_flat[:i+1]))
-
-    #precision = len(true_positives) / (len(true_positives) + len(false_positives))
-    precision = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_positives))
-    recall = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_negatives))
-    #recall = sum(map(lambda e: e.score,true_positives)) / (len(true_positives) + len(false_negatives))
-    print(recall)
-    predicted = preds_flat[i].label is not None and preds_flat[i].label.predicted
-
-    row = {"predicted":predicted,"precision":precision, "recall": recall}
-    precision_list.append(row)
-
-#print(precision_list)
+            for i in range(len(precision_list)):
+                r = filter(lambda e: e["recall"] >= precision_list[i]["recall"], precision_list)
+                r = max(r, key = lambda e: e["precision"])
+                precision_list[i]["precision_inter"] = r["precision"]
 
 
-if(CURVE_SMOOTHING):
-    precision_list[0]["smooth"] = precision_list[0]["precision"]
-    for i in range(1, len(precision_list)-1):
-        '''
-        avg = 0
-        for j in range(-2,2):
-            avg = avg+precision_list[i-j]["precision"]
-        avg = avg/5
-        '''
-        avg = (precision_list[i-1]["precision"]*10+precision_list[i]["precision"]+precision_list[i+1]["precision"]*10)/21
-
-        precision_list[i]["smooth"] = avg
-    precision_list[-1]["smooth"] = precision_list[-1]["precision"]
+        #print(precision_list)
 
 
-    for e in precision_list:
-        e["precision"] = e["smooth"]
+        #calc auc
+        drop_list = []
+        last = 2
+        for i in range(len(precision_list)):
+            if precision_list[i]["precision_inter"] < last:
+                drop_list.append(precision_list[i])
+                last = precision_list[i]["precision_inter"]
 
-#do interpolation
-
-if(POINT_INTER):
-    skip = POINT_INTER_SKIP
-    for r in np.arange(0,1,skip):
-        max =0
-        rows = [x for x in precision_list if( x["recall"] > r and x["recall"] <= (r + skip))]
-        for row in rows:
-            if row["precision"]> max: max = row["precision"]
-        for row in rows:
-            row["precision_inter"] = max
-else:
-    for i in range(len(precision_list)):
-        r = filter(lambda e: e["recall"] >= precision_list[i]["recall"], precision_list)
-        r = max(r, key = lambda e: e["precision"])
-        precision_list[i]["precision_inter"] = r["precision"]
+        auc = 0
+        for i in range(1,len(drop_list)):
+            r1 = drop_list[i]["recall"]
+            r2 = drop_list[i-1]["recall"]
+            p = drop_list[i-1]["precision_inter"] #Here we miht want to use just percision instead
+            auc += (r1-r2)*p
 
 
-#print(precision_list)
+        print(f"AUC {auc}")
 
+        #Plot
+        pres=[]
+        inter=[]
+        rec = []
+        for e in precision_list:
+            pres.append(e["precision"])
+            inter.append(e["precision_inter"])
+            rec.append(e["recall"])
 
-#calc auc
-drop_list = []
-last = 2
-for i in range(len(precision_list)):
-    if precision_list[i]["precision_inter"] < last:
-        drop_list.append(precision_list[i])
-        last = precision_list[i]["precision_inter"]
+        plt.plot(rec,pres)
+        plt.plot(rec,inter)
+        plt.savefig('filename.pdf', dpi=800)
+        plt.show()
 
-auc = 0
-for i in range(1,len(drop_list)):
-    r1 = drop_list[i]["recall"]
-    r2 = drop_list[i-1]["recall"]
-    p = drop_list[i-1]["precision_inter"] #Here we miht want to use just percision instead
-    auc += (r1-r2)*p
+    else:
+        # LRP
+        # https://arxiv.org/pdf/1807.01696.pdf
 
+        tau = IOU_THRESH
+        s = PROB_THRESHOLD
 
-print(f"AUC {auc}")
+        true_positives = list(filter(lambda e: e.prediction.prob >= s, true_positives))
+        false_negatives = list(filter(lambda e: e.prediction.prob < s, true_positives)) + false_negatives
 
-#Plot
-pres=[]
-inter=[]
-rec = []
-for e in precision_list:
-    pres.append(e["precision"])
-    inter.append(e["precision_inter"])
-    rec.append(e["recall"])
+        lrp_iou = 0
+        for e in true_positives:
+            lrp_iou += (1 - e.iou) / (1 - tau)
+        LRP = (lrp_iou + len(false_negatives) + len(false_positive)) / \
+              (len(true_positives) + len(false_positive) + len(false_negatives))
 
-plt.plot(rec,pres)
-plt.plot(rec,inter)
-plt.savefig('filename.pdf', dpi=800)
-plt.show()
+        LRPs.append((LRP, s))
+        print("LRP:", LRP, s)
 
-
+if (USE_oLRP):
+    print("LRPs (LRP, probability_treshold):")
+    print(LRPs)
+    print("optimal LRP:", min(LRPs, key=lambda e: e[0]))
 
 df = pd.DataFrame([o.__dict__ for o in labels_flat])
+
 
 print(f"Species: , # correctly predicted : , #correctly predicted with more specific class: , # Predicted with less specific class")
 
@@ -406,16 +449,3 @@ for species in species_names:
     print( f"{species} & {count_correct} & {count_offset_child} & {count_offset_parent} \\\\")
 
 pass
-
-
-
-'''
-
-import numpy as np
-rec = [0.0, 0.00, 0.10, 0.15, 0.15, 0.25, 0.25, 0.35, 0.45, 0.45, 0.55, 0.55]
-pres = [1,0.6,0.75,0.9,0.75,0.8,0.85,0.8,0.7,0.75,0.6,0.65]
-plt.plot(rec,pres)
-plt.savefig('lol.png', dpi=800)
-plt.show()
-print(rec)
-'''
